74590d9c74f91267427bfac8e8ce66c3d58ac9e8
Danila Kanapatski <danila_konopatskiy@mail.ru>
2022-11-24 20:58:35 +0300

    fixed scripts

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ const renderError = () => {
   userContainer.innerHTML = "Error...";
 };
 
+const renderUser = (user) => {
+    const { title, first, last } = user.name;
+    const { phone, email, picture } = user;
+
+    const fullName = `${title} ${first} ${last}`;
+
+    userContainer.innerHTML = `
+        <h4>${fullName}</h4>
+        <p>
+            <img alt="${fullName} avatar" src="${picture.large}"/>
+            <br/>
+            <strong>Email:</strong> ${email}
+            <br/>
+            <strong>Phone:</strong> ${phone}
+        </p>
+    `;
+};
+
 (async () => {
   try {
     renderLoading();
