2bd0915ed992556232753630ae745a54fa5aa2a5
Danila Kanapatski <danila_konopatskiy@mail.ru>
2022-11-24 20:50:22 +0300

    fix

--- script.js
+++ script.js
@@ -12,24 +12,6 @@ const renderError = () => {
   userContainer.innerHTML = "Error...";
 };
 
-const renderUser = (user) => {
-  const { title, first, last } = user.name;
-  const { phone, email, picture, gender } = user;
-
-  userContainer.innerHTML = `
-        <h4>${title} ${first} ${last}</h4>
-        <p>
-            <img alt="${title} ${first} ${last} avatar" src="${picture.large}"/>
-            <br/>
-            <strong>Gender:</strong> ${gender}
-            <br/>
-            <strong>Email:</strong> ${email}
-            <br/>
-            <strong>Phone:</strong> ${phone}
-        </p>
-    `;
-};
-
 (async () => {
   try {
     renderLoading();
